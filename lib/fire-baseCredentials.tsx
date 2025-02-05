d1517d587d7bd7ece5195d522994851d2547e959
Pawan Hettiarachchi <pwnkanishka@gmail.com>
2025-02-05 13:25:11 +0530

    Create fire-baseCredentials.tsx

---
+++ lib/fire-baseCredentials.tsx
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAvmiCQzZYm6iKBuG7G61n0N9v71CIhbSQ",
+    authDomain: "baecon-iscommunity.firebaseapp.com",
+    projectId: "baecon-iscommunity",
+    storageBucket: "baecon-iscommunity.firebasestorage.app",
+    messagingSenderId: "984787874932",
+    appId: "1:984787874932:web:7c348078b195415cf14d21",
+    databaseURL: "https://baecon-iscommunity-default-rtdb.asia-southeast1.firebasedatabase.app"
+  };
